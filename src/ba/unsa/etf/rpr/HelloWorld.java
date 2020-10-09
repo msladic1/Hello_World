df82dc178c14c7e1607b4b888af0996c873c0cd5
msladic1 <msladic1@etf.unsa.ba>
2020-10-09 15:52:03 +0200

    Malo igranja sa jednostavnim stringovima

--- src/ba/unsa/etf/rpr/HelloWorld.java
+++ src/ba/unsa/etf/rpr/HelloWorld.java
@@ -3,5 +3,8 @@ package ba.unsa.etf.rpr;
 public class HelloWorld {
     public static void main(String[] args) {
     System.out.println("Hello World!\nMuris Sladic");
+    String s = "Zdravo";
+    System.out.println("Rijec " + s + " ima " + s.length() + " slova.\nLajhmaninanet");
+    s = null;
     }
 }
