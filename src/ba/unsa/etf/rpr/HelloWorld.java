bb71192188cad8beb3fe982e2f11e575116a6fd4
msladic1 <msladic1@etf.unsa.ba>
2020-10-09 15:35:20 +0200

    Novo ime klase

--- src/ba/unsa/etf/rpr/HelloWorld.java
+++ src/ba/unsa/etf/rpr/HelloWorld.java
@@ -1,7 +1,6 @@
 package ba.unsa.etf.rpr;
 
-public class Main {
-
+public class HelloWorld {
     public static void main(String[] args) {
     System.out.println("Hello World!\nMuris Sladic");
     }
